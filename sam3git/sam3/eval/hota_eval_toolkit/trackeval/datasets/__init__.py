1bc1ddbca38d4449f6cd8396c04f623e349209d0
XINYOU <n126992630@gmail.com>
2026-05-03 14:13:15 +0800

    remove sam3 github code

--- sam3git/sam3/eval/hota_eval_toolkit/trackeval/datasets/__init__.py
+++
@@ -1,6 +0,0 @@
-# flake8: noqa
-
-# pyre-unsafe
-
-from .tao_ow import TAO_OW
-from .youtube_vis import YouTubeVIS
--- sam3git/sam3/eval/teta_eval_toolkit/datasets/__init__.py
+++
@@ -1,7 +0,0 @@
-# fmt: off
-# flake8: noqa
-
-# pyre-unsafe
-"""Datasets."""
-from .coco import COCO
-from .tao import TAO
